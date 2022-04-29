4cd8d5eed9a5734a8f0cdb15565b14010dcf59c4
Matheus C <matheuschaves21@gmail.com>
2022-04-29 11:32:07 -0300

    feat: create database

---
+++ src/db/database.js
@@ -0,0 +1,10 @@
+const products = [
+  { name: 'Bicicleta Ever Azul MTB 29 2021', price: 1790, image: 'https://img.imageboss.me/semexe-aws/cover:contain/1200x900/dpr:2,format:webp/images/detailed/159/semexe-o-melhor-lugar-do-brasil-para-comprar-e-vender-sua-bike-6144b0161e009-IMG-20210810-125734-274-scaled.jpg' },
+  { name: 'Bicicleta Aro 29 21v Flexus Free Action', price: 1099.9, image: 'https://static.ebomdecomprar.com.br/ebomdecomprar/5f6b2d065257320200923110958.jpeg' },
+  { name: 'Bicicleta Wild 12V Cassete 11x52 (Preto e Cinza)', price: 4690.6, image: 'https://cdnv2.moovin.com.br/bpbikeshop/imagens/produtos/original/bicicleta-wild-12v-cassete-11x52-preto-e-cinza-absolute-b6479beb21f6a83f3edb51f14f27eabe.jpeg' },
+  { name: 'Bicicleta Aro 26 DP 21 Marchas - Colli Bike', price: 1137, image: 'https://cdn.sistemawbuy.com.br/arquivos/9868bc0a705df9918e5111114d0835f0/produtos/PAE7POU5/5-60e76106f1fb7.jpg' },
+  { name: 'Bicicleta Active Shimano 21V (Preto e Azul) - Kode', price: 2950.18, image: 'https://cdnv2.moovin.com.br/bpbikeshop/imagens/produtos/original/bicicleta-active-shimano-21v-preto-e-azul-kode-9e71a0b5190a45928f1310f83f1ae31e.png' },
+  { name: 'Bicicleta Redstone Macropus Aro 29 Shimano Alivio 18v Freio a Disco Hidraulico Suspensao com Trava Prata e Preta', price: 5509.05, image: 'https://static3.tcdn.com.br/img/img_prod/266841/bicicleta_redstone_macropus_aro_29_shimano_alivio_18v_freio_a_disco_hidraulico_suspensao_com_trava_p_17077_1_3d2cb4effad9d1d4aa9d63debc77cebc_20220212190027.jpg' },
+]
+
+export { products }
