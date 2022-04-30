a0bf33f7506ca2776aa648021a23ed71b4c6dd7b
Matheus C <matheuschaves21@gmail.com>
2022-04-30 20:41:03 -0300

    feat: add title to home page

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useSelector } from "react-redux"
 import {
-  Flex
+  Flex, Heading
 } from "@chakra-ui/react"
 
 import { ProductList } from '../../components/ProductList'
@@ -17,6 +17,16 @@ const Products = () => {
       direction="column"
       minHeight='100vh'
     >
+      <Heading
+        as='h1'
+        color='gray.700'
+        mt={125}
+        mb={10}
+        textAlign='center'
+        textShadow='2px 2px 0 black'
+      >
+        🚴 BICICLETAS ⬇
+      </Heading>
       <ProductList products={products} setProduct={setProduct} />
       <Showcase product={product} mt={50} mb={100} p={10} />
     </Flex>
